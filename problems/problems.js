b8de881944e59964083be52ebd6c7a680b247d51
maddularamkumar223 <maddularamkumar223@gmail.com>
2026-03-30 08:42:14 +0530

    Upadte all the codes

--- problems/problems.js
+++ problems/problems.js
@@ -1,12 +1,11 @@
 let problem_statement = document.querySelector(".problem_statement");
 let currentIndex = 0;
 let dataDisplay = document.querySelector(".dataDisplay");
+let userIdProblem = sessionStorage.getItem("id");
 
 let problems = async () => {
   let response = await fetch("http://localhost:3000/problems");
   let responseData = await response.json();
-  console.log(responseData);
-
   let backButton = document.createElement("button");
   let fwdButton = document.createElement("button");
   let buttonContainer = document.createElement("div");
@@ -18,46 +17,77 @@ let problems = async () => {
   problem_statement.append(buttonContainer);
 
   fwdButton.addEventListener("click", () => {
-    if (currentIndex < responseData.length) {
+    if (currentIndex < responseData.length - 1) {
       currentIndex++;
       let question = responseData[currentIndex];
-      console.log(question);
       displayProblem(question);
+      console.log(currentIndex);
     } else {
-      let h1 = document.createElement("h1");
-      h1.innerHTML = "Data Not Found";
-      problem_statement.append(h1);
+      fwdButton.style.disabled = "true";
+      fwdButton.style.backgroundColor = "transparent";
+      fwdButton.style.color = "gray";
     }
   });
+
   backButton.addEventListener("click", () => {
     currentIndex = currentIndex - 1;
-
-    if (currentIndex < responseData.length) {
-      let question = responseData[currentIndex];
-      console.log(question);
-      displayProblem(question);
+    if (currentIndex === 1) {
+      backButton.style.disabled = "true";
+      backButton.style.backgroundColor = "transparent";
+      backButton.style.color = "gray";
     } else {
-      let h1 = document.createElement("h1");
-      h1.innerHTML = "Data Not Found";
-      problem_statement.append(h1);
+      let question = responseData[currentIndex];
+      displayProblem(question);
     }
   });
-
-  console.log(currentIndex);
+  displayProblem(responseData[0]);
 };
 problems();
 
 let displayProblem = (data) => {
-  let questionNO = document.createElement("p");
-  let title = document.createElement("p");
-  let description = document.createElement("p");
-  let level = document.createElement("p");
-  let container = document.createElement("aside");
-  container.className = "dataDisplay";
+  let questionNO = document.querySelector(".questionNo");
+  let title = document.querySelector(".title");
+  let description = document.querySelector(".description");
+  let level = document.querySelector(".level");
   questionNO.innerHTML = `Question No : ${data.problem_id}`;
   title.innerHTML = `Title: ${data.title}`;
   description.innerHTML = `Description: ${data.description} `;
-  level.innerHTML = `Level: ${data.difficulty}`;
-  container.append(questionNO, title, level, description);
-  problem_statement.append(container);
+  level.innerHTML = `Level: <span>${data.difficulty}</span>`;
+};
+
+let updateQuestion = async () => {
+  let response = await fetch(`http://localhost:3000/users/${userIdProblem}`);
+  let data = await response.json();
+  let level = document.querySelector("span").innerText;
+  if (level === "Easy") {
+    let updateEasy = data.easy + 1;
+    questionUpdate({ easy: updateEasy });
+  } else if (level === "Medium") {
+    let updateMedium = data.medium + 1;
+    questionUpdate({ medium: updateMedium });
+  } else if (level === "Hard") {
+    let updateHard = data.hard + 1;
+    questionUpdate({ hard: updateHard });
+  }
+};
+
+let solveButton = document
+  .querySelector(".solveButton")
+  .addEventListener("click", async () => {
+    console.log("Button Clicked");
+    await updateQuestion();
+  });
+
+let questionUpdate = async (level) => {
+  try {
+    await fetch(`http://localhost:3000/users/${userIdProblem}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(level),
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
